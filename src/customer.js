323446b6d420ade085857e2d527c9123650ef1f8
望月由登 <mochidzukiyutou@mochidzukiyutounoMacBook-puro.local>
2019-01-08 00:44:18 +0900

    ヘッダ部分を修正

--- src/customer.js
+++ src/customer.js
@@ -19,6 +19,42 @@ const customer = new Vue({
                 department: '営業本部 第一営業部',
                 email: 'suma@sumawaku.com',
                 telNo: '123-456-7890'
+            },
+            relation: {
+                project: [
+                    {
+                        code: 'P-000001',
+                        projectName: '初期システム開発',
+                        charge: '須磨和久',
+                        startDate: '2018-12-01',
+                        endDate: '2019-02-28',
+                        sales: '20,000'
+                    },
+                    {
+                        code: 'P-000001',
+                        projectName: '初期システム開発',
+                        charge: '須磨和久',
+                        startDate: '2018-12-01',
+                        endDate: '2019-02-28',
+                        sales: '20,000'
+                    }
+                ],
+                customerDealing: [
+                    {
+                        date: '2018-12-31',
+                        kind: '問合せ対応',
+                        charge: '須磨和久',
+                        question: 'どうやったら利用を開始できますか？',
+                        answer: '下記のホームページを参考にしてください。https://google.com/'
+                    },
+                    {
+                        date: '2018-12-31',
+                        kind: '問合せ対応',
+                        charge: '須磨和久',
+                        question: 'どうやったら利用を開始できますか？',
+                        answer: '下記のホームページを参考にしてください。https://google.com/'
+                    }
+                ]
             }
         }
     },
